12c59102ed30116dc95b263ad1fd8adf98949c1c
Mikael Cassamassimo Borges <66427859+yoskatista@users.noreply.github.com>
2021-02-18 11:52:31 -0300

    Create tuplas e modificação.py

---
+++ tuplas e modificação.py
@@ -0,0 +1,8 @@
+pratos = ('ArrozFeijão','Macarrão','Feijoada','Omelete')
+for pratosTuplas in pratos:
+    print(pratosTuplas)
+
+pratos = ('feijãoArros','Macarone','Feijo','Ovocelent')
+print('\nlista modificada')
+for prato in pratos:                                            #Dessa forma modificamos uma tupla
+    print(prato)
